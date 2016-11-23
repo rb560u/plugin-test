17813ea3edc7d2b566758d31a33baf204a5c6780
Felipe Monteiro <felipe.monteiro@att.com>
2016-11-23 10:22:23 -0500

    Added namespace client/tests. Removed contrail_client.

---
+++ contrail_tempest_plugin/services/contrail/json/namespace_client.py
@@ -0,0 +1,68 @@
+# Copyright 2016 AT&T Corp
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+
+from contrail_tempest_plugin.services.contrail.json import base
+from six.moves.urllib import parse as urllib
+from tempest.common.utils import data_utils
+
+
+class NamespaceClient(base.BaseContrailClient):
+
+    def list_namespaces(self, params=None):
+        url = '/namespaces'
+        if params:
+            url += '?%s' % urllib.urlencode(params)
+        return self.get(url)
+
+    def create_namespace(self, fq_name=None, **kwargs):
+        if fq_name is None:
+            fq_name = data_utils.rand_name('namespace')
+        url = '/namespaces'
+        post_body = {
+            'namespace': {
+                'parent_type': 'domain',
+                'fq_name': ['default-domain', fq_name]
+            }
+        }
+        if kwargs:
+            post_body['namespace'].update(kwargs)
+
+        resp, body = self.post(url, json.dumps(post_body))
+        resp_body = json.loads(body)
+
+        if 'namespace' in resp_body:
+            return resp, resp_body['namespace']['uuid']
+        return resp, None
+
+    def show_namespace(self, uuid):
+        url = '/namespace/{0}'.format(uuid)
+        return self.get(url)
+
+    def update_namespace(self, uuid, **kwargs):
+        url = '/namespace/{0}'.format(uuid)
+        put_body = {
+            'namespace': {
+                'display_name': data_utils.rand_name('namespace')
+            }
+        }
+        if kwargs:
+            post_body['namespace'].update(kwargs)
+        return self.put(url, json.dumps(put_body))
+
+    def delete_namespace(self, uuid):
+        url = '/namespace/{0}'.format(uuid)
+        return self.delete(url)
---
+++ contrail_tempest_plugin/tests/api/contrail/test_namespace.py
@@ -0,0 +1,96 @@
+# Copyright 2016 AT&T Corp
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_log import log as logging
+
+from contrail_tempest_plugin.tests.api.contrail import base
+
+from tempest.common.rbac import rbac_rule_validation
+from tempest.common.rbac.rbac_utils import rbac_utils
+
+from tempest import test
+
+LOG = logging.getLogger(__name__)
+
+
+class NamespaceContrailTest(base.BaseContrailTest):
+
+    @test.attr(type='rbac')
+    @rbac_rule_validation.action(component="Contrail",
+                                 rule="list_namespaces")
+    def test_list_namespaces(self):
+        rbac_utils.switch_role(self, switchToRbacRole=True)
+        try:
+            self.namespace_client.list_namespaces()
+        finally:
+            rbac_utils.switch_role(self, switchToRbacRole=False)
+
+    @test.attr(type='rbac')
+    @rbac_rule_validation.action(component="Contrail",
+                                 rule="create_namespace")
+    def test_create_namespace(self):
+        was_created = False
+        rbac_utils.switch_role(self, switchToRbacRole=True)
+        try:
+            resp, ns_uuid = self.namespace_client.create_namespace()
+            was_created = resp.status == 200
+        finally:
+            rbac_utils.switch_role(self, switchToRbacRole=False)
+        if was_created:
+            self.namespace_client.delete_namespace(ns_uuid)
+
+    @test.attr(type='rbac')
+    @rbac_rule_validation.action(component="Contrail",
+                                 rule="show_namespace")
+    def test_show_namespace(self):
+        resp, ns_uuid = self.namespace_client.create_namespace()
+        was_created = resp.status == 200
+        rbac_utils.switch_role(self, switchToRbacRole=True)
+        try:
+            self.namespace_client.show_namespace(ns_uuid)
+        finally:
+            rbac_utils.switch_role(self, switchToRbacRole=False)
+            if was_created:
+                self.namespace_client.delete_namespace(ns_uuid)
+
+    @test.attr(type='rbac')
+    @rbac_rule_validation.action(component="Contrail",
+                                 rule="update_namespace")
+    def test_update_namespace(self):
+        resp, ns_uuid = self.namespace_client.create_namespace()
+        was_created = resp.status == 200
+        rbac_utils.switch_role(self, switchToRbacRole=True)
+        try:
+            self.namespace_client.update_namespace(ns_uuid)
+        finally:
+            rbac_utils.switch_role(self, switchToRbacRole=False)
+        if was_created:
+            self.namespace_client.delete_namespace(ns_uuid)
+
+    @test.attr(type='rbac')
+    @rbac_rule_validation.action(component="Contrail",
+                                 rule="delete_namespace")
+    def test_delete_namespace(self):
+        resp, ns_uuid = self.namespace_client.create_namespace()
+        was_created = resp.status == 200
+        was_deleted = False
+        rbac_utils.switch_role(self, switchToRbacRole=True)
+        try:
+            resp, _ = self.namespace_client.delete_namespace(ns_uuid)
+            was_deleted = resp.status == 200
+        finally:
+            rbac_utils.switch_role(self, switchToRbacRole=False)
+        if was_created and not was_deleted:
+            self.namespace_client.delete_namespace(ns_uuid)
